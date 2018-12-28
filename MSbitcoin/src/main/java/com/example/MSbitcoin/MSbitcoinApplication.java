cb75044dd24e98c5c51ce05b2b9a95fb6624646e
dragandulic <35114903+dragandulic@users.noreply.github.com>
2018-12-28 16:15:11 +0100

    Eureka server created

---
+++ MSbitcoin/src/main/java/com/example/MSbitcoin/MSbitcoinApplication.java
@@ -0,0 +1,14 @@
+package com.example.MSbitcoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MSbitcoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MSbitcoinApplication.class, args);
+	}
+
+}
+
