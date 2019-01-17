786aab482f71606cba4685120d805e854b821ba9
jurke95 <markivicius@hotmail.com>
2019-01-17 01:16:36 +0100

    initial paypal

---
+++ MSpaypal/src/main/java/com/example/MSpaypal/controller/PayPalDTO.java
@@ -0,0 +1,45 @@
+package com.example.MSpaypal.controller;
+
+public class PayPalDTO {
+
+	
+	
+	private String sum;
+	
+	
+	
+
+	
+	
+	public PayPalDTO(){
+		
+	}
+	
+	
+	
+
+	public PayPalDTO(String sum) {
+		super();
+		this.sum = sum;
+	}
+
+	public String getSum() {
+		return sum;
+	}
+
+	public void setSum(String sum) {
+		this.sum = sum;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ MSpaypal/src/main/java/com/example/MSpaypal/service/PayPalService.java
@@ -0,0 +1,125 @@
+package com.example.MSpaypal.service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Service;
+
+import com.paypal.api.payments.Amount;
+import com.paypal.api.payments.Links;
+import com.paypal.api.payments.Payer;
+import com.paypal.api.payments.Payment;
+import com.paypal.api.payments.PaymentExecution;
+import com.paypal.api.payments.RedirectUrls;
+import com.paypal.api.payments.Transaction;
+import com.paypal.base.rest.APIContext;
+import com.paypal.base.rest.PayPalRESTException;
+
+@Service
+public class PayPalService {
+	
+	
+	String clientId = "AYXs64yPx_es9JkYpmKDqXfFvcGkQ9PQc8GJhbXX-Q-TDxEFDy8TQfUSJrhHlixFxSW3X1rBq6y2tGL6"; //markivicius@hotmail.com
+	String clientSecret = "EGFJGDJ6dvWZqR4I8844WufT2XZi_3pZq7o-93b9PeBV5Ms-GDyDbBZnm-rF1WsMUZEIxFIZGoHKimlr";  //!paypalsep
+	
+	
+	
+	public Map<String, Object> createPayment(String sum){
+	    Map<String, Object> response = new HashMap<String, Object>();
+	    Amount amount = new Amount();
+	    amount.setCurrency("USD");
+	    amount.setTotal(sum);
+	    Transaction transaction = new Transaction();
+	    transaction.setAmount(amount);
+	    List<Transaction> transactions = new ArrayList<Transaction>();
+	    transactions.add(transaction);
+
+	    Payer payer = new Payer();
+	    payer.setPaymentMethod("paypal");
+
+	    Payment payment = new Payment();
+	    payment.setIntent("sale");
+	    payment.setPayer(payer);
+	    payment.setTransactions(transactions);
+
+	    RedirectUrls redirectUrls = new RedirectUrls();
+	    redirectUrls.setCancelUrl("http://localhost:3000/cancel");
+	    redirectUrls.setReturnUrl("http://localhost:3000/");
+	    payment.setRedirectUrls(redirectUrls);
+	    Payment createdPayment;
+	    try {
+	        String redirectUrl = "";
+	        APIContext context = new APIContext(clientId, clientSecret, "sandbox");
+	        createdPayment = payment.create(context);
+	        if(createdPayment!=null){
+	            List<Links> links = createdPayment.getLinks();
+	            for (Links link:links) {
+	                if(link.getRel().equals("approval_url")){
+	                    redirectUrl = link.getHref();
+	                    break;
+	                }
+	            }
+	            response.put("status", "success");
+	            response.put("redirect_url", redirectUrl);
+	        }
+	    } catch (PayPalRESTException e) {
+	        System.out.println("Error happened during payment creation!");
+	    }
+	    return response;
+	}
+	
+	
+	
+	public Map<String, Object> completePayment(HttpServletRequest req){
+	    Map<String, Object> response = new HashMap();
+	    Payment payment = new Payment();
+	    payment.setId(req.getParameter("paymentId"));
+	    
+
+	    PaymentExecution paymentExecution = new PaymentExecution();
+	    paymentExecution.setPayerId(req.getParameter("PayerID"));
+	    try {
+	        APIContext context = new APIContext(clientId, clientSecret, "sandbox");
+	        Payment createdPayment = payment.execute(context, paymentExecution);
+	        if(createdPayment!=null){
+	            response.put("status", "success");
+	            response.put("payment", createdPayment);
+	        }
+	    } catch (PayPalRESTException e) {
+	        System.err.println(e.getDetails());
+	    }
+	    return response;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
